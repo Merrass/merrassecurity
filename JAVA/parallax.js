25691c0e182728aaf6bd78dda9854979f1cf0858
Merras <120323187+Merrass@users.noreply.github.com>
2025-08-29 20:44:23 +0200

    Add files via upload
    Uploading my web files.

---
+++ JAVA/parallax.js
@@ -0,0 +1,6 @@
+// JavaScript code for parallax effect (assuming library is named "Parallax")
+
+window.onload = function() {
+    // Initialize parallax effect on the element with ID "scene"
+    new Parallax(document.getElementById('scene'));
+};
